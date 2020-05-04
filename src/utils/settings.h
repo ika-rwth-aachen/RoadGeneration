124752ddb412b554b11eee1d6131951c7d02f609
Christian Geller <christian.geller@rwth-aachen.de>
2020-05-04 20:50:27 +0200

    Add settings file

---
+++ src/utils/settings.h
@@ -0,0 +1,15 @@
+// file settings.h
+
+struct laneWidths
+{
+    double access = 3;
+    double main = 4;
+    double standard = 3.5;
+};
+
+struct laneSpeeds
+{
+    double access = 30;
+    double main = 50;
+    double standard = 50;
+};
