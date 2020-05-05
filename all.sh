f4a6a5f1db5f2c2500564b43fe3f6353d30f23b9
Christian Geller <christian.geller@rwth-aachen.de>
2020-05-05 15:06:18 +0200

    Update input files to ensure full functionality

--- all.sh
+++ all.sh
@@ -1,7 +1,7 @@
-./road-generation io/all.xml
 ./road-generation io/2M.xml
 ./road-generation io/M2A.xml
 ./road-generation io/4A.xml
+./road-generation io/roundabout.xml
 ./road-generation io/junction1.xml
 ./road-generation io/junction2.xml
 ./road-generation io/junction3.xml
@@ -9,3 +9,4 @@
 ./road-generation io/junction5.xml
 ./road-generation io/junction6.xml
 ./road-generation io/junction7.xml
+./road-generation io/all.xml
