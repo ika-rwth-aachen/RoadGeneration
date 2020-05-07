e94876310e26f40d3db7155f07c2677f585e2eea
Christian Geller <christian.geller@rwth-aachen.de>
2020-05-07 19:03:54 +0200

    Update xsd docu style

--- doc/xsdDoc.sh
+++ doc/xsdDoc.sh
@@ -1,3 +1,3 @@
 # requires an XSLT tool such as xsltproc (preinstalled on Mac OS)
 
-xsltproc --stringparam title "Road Generation Input Documentation" xs3p.xsl ../xml/input.xsd > xsdDocumentation.html
+xsltproc --stringparam title "Road Generation Input Documentation" xs3p_2.xsl ../xml/input.xsd > xsdDocumentation.html
