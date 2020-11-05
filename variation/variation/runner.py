c758ab965817a0962907feddae1f612136057ed1
Jannik Busse <jannik.busse@rwth-aachen.de>
2020-11-05 16:46:03 +0100

    minor changes

--- variation/variation/runner.py
+++ variation/variation/runner.py
@@ -117,6 +117,7 @@ def executePipeline(n, tree, inpDir, nwName, varDict):
         
         libpath = os.path.abspath("../build/libroadgen.so")  
         if os.name == "posix":  # if MacOS
+            
             roadgen = cdll.LoadLibrary(libpath) #load shared lib
             arg = c_char_p(tmpName.encode('utf-8')) #execute "main" function from lib
             logarg = c_char_p("data/logg.txt".encode("utf-8"))
