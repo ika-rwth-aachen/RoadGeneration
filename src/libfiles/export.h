6da7357919403f0192fbaef4ab3462258b6aed9e
Jannik <Jannik@DESKTOP-GAFK960>
2020-12-09 13:46:05 +0100

    fixed export header

--- src/libfiles/export.h
+++ src/libfiles/export.h
@@ -2,11 +2,7 @@
 #define EXPORT_H
 
 #ifdef _WIN32
-# ifdef WIN_EXPORT
 #   define EXPORTED  __declspec( dllexport )
-# else
-#   define EXPORTED  __declspec( dllimport )
-# endif
 #else
 # define EXPORTED
 #endif
