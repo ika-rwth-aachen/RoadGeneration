87fca2962e9db4c1dc794b38d8420b0a13a55815
Daniel Becker <daniel.becker@ika.rwth-aachen.de>
2020-12-17 10:13:51 +0100

    Update buildScript.sh

--- buildScript.sh
+++ buildScript.sh
@@ -7,6 +7,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     rm -rf *
     cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../bin ..
     cmake --build .
+    cd ..
 else
     echo "Windows?"
     rm -r build 
@@ -16,4 +17,5 @@ else
     cmake -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX=../bin -DXercesC_INCLUDE_DIR="../libs/xerces-c-3.2.3/bin/include" -DXercesC_LIBRARY="../libs/xerces-c-3.2.3/bin/lib/xerces-c_3" ..
     # make
     cmake --build . --config Release
+    cd ..
 fi 
