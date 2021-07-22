1f076a79775201da784a8b42e8138d5eb051736f
Jannik Busse <jannik.busse@rwth-aachen.de>
2021-07-22 16:58:06 +0200

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# road-generation
-Generation of Complex Road Networks Using a Simplified Logical Description for the Validation of Automated Vehicles
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git submodule update --init
-mkdir -p build
-cd build
-rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../bin ..
-cmake --build .
-cd ..
