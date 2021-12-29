fccc0a44f2b558ab07e464c1b6de87f98a6629b2
Jannik Busse <jannik.busse@rwth-aachen.de>
2021-12-29 19:31:54 +0100

    reworked doxygen config

--- doc/xsdDocumentation.sh
+++
@@ -1,3 +0,0 @@
-# requires an XSLT tool such as xsltproc (preinstalled on Mac OS)
-
-xsltproc --stringparam title "Road Generation Input Documentation" xs3p_2.xsl ../xml/input.xsd > xsdDocumentation.html
